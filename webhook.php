92940214dad117650c3f9d3035568b20e9a86e58
Dylan Balouza <dbalouza@gmail.com>
2016-04-26 11:48:19 +0000

    right path

--- webhook.php
+++ webhook.php
@@ -7,5 +7,10 @@ if ($verify_token === 'CrossR0@ds123') {
   echo $challenge;
 }
 
-$input = json_decode(file_get_contents('php://input'), true);
+$input = json_decode(file_get_contents('php://input'), true, 512, JSON_BIGINT_AS_STRING);
 error_log(print_r($input, true));
+
+$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
+
+error_log(print_r($leadgen_id, true));
+?>
